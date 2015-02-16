ad0ee8f97924b4ca97d4d83f5b8d6120ae9caddd
Tarik Hamilton <tarikhamilton@gmail.com>
2015-02-16 13:50:17 -0500

    Quiz count works, shit is random too

--- client/main/main.js
+++ client/main/main.js
@@ -7,6 +7,17 @@ Template.study.helpers({
 var getPathname = function() { return window.location.pathname.replace(/^.*[\\\/]/g, ''); }
 
 currentQuestionSet = {};
+quiz = {
+	score: 0,
+	answered: 0,
+	accuracy: function () {
+		return Math.round(this.score / this.answered * 100) + '%';
+	},
+	reset: function() {
+		this.score = 0;
+		this.answered = 0;
+	}
+};
 
 Template.mainLayout.helpers({
 	currentSet: getPathname()
@@ -20,7 +31,7 @@ Template.quiz.helpers({
 });
 
 Template.quiz.events({
-	'click #quiz-controls a' : function(e) {
+	'click #quiz #controls a' : function(e) {
 		e.preventDefault;
 		var total = currentQuestionSet.length;
 		var randomIndex = Math.floor(Math.random() * total);
@@ -29,5 +40,21 @@ Template.quiz.events({
 		$('#quiz-question').html(question);
 		$('#quiz-answer').html(answer);
 		MathJax.Hub.Typeset();
+	},
+	'click #quiz #controls #correct' : function() {
+		quiz.score++;
+		quiz.answered++;
+		$('#score #accuracy').html(quiz.accuracy());
+		$('#score #total').html(quiz.answered);
+	},
+	'click #quiz #controls #wrong' : function() {
+		quiz.answered++;
+		$('#score #accuracy').html(quiz.accuracy());
+		$('#score #total').html(quiz.answered);
+	},
+	'click #quiz #controls #reset' : function() {
+		quiz.reset();
+		$('#score #accuracy').html(quiz.score);
+		$('#score #total').html(quiz.answered);
 	}
 })

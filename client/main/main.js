1bce64ebde4ff9f36f8e6bf1463da8c14c4a119e
Tarik Hamilton <tarikhamilton@gmail.com>
2015-02-18 01:33:56 -0500

    jQuery to JavaScript on main quiz object. Refactor too

--- client/main/main.js
+++ client/main/main.js
@@ -5,13 +5,50 @@ quiz = {
 	score: 0,
 	answered: 0,
 	accuracy: function () {
-		return Math.round(this.score / this.answered * 100) + '%';
+		if (this.answered != 0)
+			return Math.round(this.score / this.answered * 100) + '%';
+		else
+			return '---%';
 	},
 	reset: function() {
 		this.score = 0;
 		this.answered = 0;
+		document.getElementById('quiz-score-accuracy').innerHTML = '---%';
+		document.getElementById('quiz-score-total').innerHTML = 0;
+	},
+	update: function() {
+		document.getElementById('quiz-score-accuracy').innerHTML = this.accuracy();
+		document.getElementById('quiz-score-total').innerHTML = this.answered;
 	}
 };
+
+quiz.newQuestion = function() { 
+	document.getElementById('quiz-answer').style.display = 'none';
+	var total = currentQuestionSet.length;
+	var randomIndex = Math.floor(Math.random() * total);
+	var question = '$' + currentQuestionSet[randomIndex].integral + '$';
+	var answer = '$' + currentQuestionSet[randomIndex].differential + '$';
+	document.getElementById('quiz-question').innerHTML = question;
+	document.getElementById('quiz-answer').innerHTML = answer;
+	MathJax.Hub.Typeset();
+	this.buttons.reset();
+	this.update();
+}
+
+quiz.buttons = {
+	check: function() {
+		document.getElementById('quiz-answer').style.display = 'inline';
+		document.getElementById('quiz-button-check').style.display = 'none';
+		document.getElementById('quiz-button-correct').style.display = 'inline';
+		document.getElementById('quiz-button-wrong').style.display = 'inline';
+	},
+	reset: function() {
+		document.getElementById('quiz-answer').style.display = 'none';
+		document.getElementById('quiz-button-check').style.display = 'inline';
+		document.getElementById('quiz-button-correct').style.display = 'none';
+		document.getElementById('quiz-button-wrong').style.display = 'none';
+	}
+}
 
 Template.mainLayout.helpers({
 	currentSet: getPathname()
@@ -31,28 +68,21 @@ Template.quiz.helpers({
 Template.quiz.events({
 	'click #quiz #controls a' : function(e) {
 		e.preventDefault;
-		var total = currentQuestionSet.length;
-		var randomIndex = Math.floor(Math.random() * total);
-		var question = '$' + currentQuestionSet[randomIndex].integral + '$';
-		var answer = '$' + currentQuestionSet[randomIndex].differential + '$';
-		$('#quiz-question').html(question);
-		$('#quiz-answer').html(answer);
-		MathJax.Hub.Typeset();
 	},
-	'click #quiz #controls #correct' : function() {
+	'click #quiz-button-check' : function() {
+		quiz.buttons.check();
+	},
+	'click #quiz-button-correct' : function() {
 		quiz.score++;
 		quiz.answered++;
-		$('#score #accuracy').html(quiz.accuracy());
-		$('#score #total').html(quiz.answered);
+		quiz.newQuestion();
 	},
-	'click #quiz #controls #wrong' : function() {
+	'click #quiz-button-wrong' : function() {
 		quiz.answered++;
-		$('#score #accuracy').html(quiz.accuracy());
-		$('#score #total').html(quiz.answered);
+		quiz.newQuestion();
 	},
-	'click #quiz #controls #reset' : function() {
+	'click #quiz-button-reset' : function() {
 		quiz.reset();
-		$('#score #accuracy').html(quiz.score);
-		$('#score #total').html(quiz.answered);
+		quiz.newQuestion();
 	}
 })
